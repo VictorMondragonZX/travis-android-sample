a0b8aea6d8167ee8ab6f0134a7560e33169cb74c
Edinofri <edinofricaniago@gmail.com>
2020-10-30 11:00:43 +0700

    build-tools 30 have issue with travis

--- app/src/androidTest/java/com/example/learntravis/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/learntravis/ExampleInstrumentedTest.kt
@@ -1,28 +1,46 @@
 package com.example.learntravis
 
-import androidx.test.core.app.ActivityScenario
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.pressBack
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 
 import org.junit.Test
-import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 import org.junit.Rule
+import java.lang.Thread.sleep
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    
+
+
+public class ExampleInstrumentedTest {
+    @get:Rule
+    public val activityTestRule: ActivityTestRule<MainActivity> = ActivityTestRule(
+        MainActivity::class.java,
+        true
+    )
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.learntravis", appContext.packageName)
+    }
+
+    @Test
+    fun testBtnNext() {
+        onView(withId(R.id.fab))
+            .perform(click())
+        onView(withText("NEXT"))
+            .perform(click())
+            .perform(pressBack())
     }
 }
